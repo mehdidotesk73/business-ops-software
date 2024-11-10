37b0fc15cf5bc81ce4a7fb3b2c12f2bb779dcc57
mehdidotesk73 <mehdi.esk73@gmail.com>
2024-11-10 13:55:21 -0700

    deployment attempt 8

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,6 +5,8 @@ import UserContext from "../components/userContext";
 function Login() {
   const { setUser } = useContext(UserContext);
 
+  console.log("Test in login");
+
   const handleLogin = (userData) => {
     setUser(userData);
     // Additional login logic
