6ed11f33686951e4a6905ee9d029b4d1f900e564
mehdidotesk73 <mehdi.esk73@gmail.com>
2024-11-10 20:08:50 -0700

    deployment attempt 13

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-// const apiUrl = "/choreo-apis/business-ops-software/backend/v1";
-const apiUrl =
-  "https://592cc7be-b783-4230-8f0e-70e83047c705-prod.e1-us-east-azure.choreoapis.dev/business-ops-software/backend/v1.0";
+const apiUrl = "/choreo-apis/business-ops-software/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
