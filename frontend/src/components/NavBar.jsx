36cad98e8825452e87e36e5e2b5ae90343a9c323
mehdidotesk73 <mehdi.esk73@gmail.com>
2024-11-11 11:11:50 -0700

    debugged Navbar collapse issue

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,24 +2,30 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import UserContext from "./userContext";
 
 const Navbar = () => {
   const { user, setUser } = useContext(UserContext);
 
   return (
-    <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
-      <Link className='navbar-brand ms-3' to='/'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark ps-3 pe-3'>
+      <Link className='navbar-brand' to='/'>
         Friends Electric
       </Link>
+
       <button
         className='navbar-toggler'
         type='button'
-        data-toggle='collapse'
-        data-target='#navbarNav'
+        data-bs-toggle='collapse'
+        data-bs-target='#navbarNavDropdown'
+        aria-controls='navbarNavDropdown'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
       >
         <span className='navbar-toggler-icon'></span>
       </button>
+
       <div className='collapse navbar-collapse' id='navbarNavDropdown'>
         <ul className='navbar-nav mr-auto'>
           <li className='nav-item active'>
