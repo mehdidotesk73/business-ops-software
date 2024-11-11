c0ea319c57ec4a1d5fe048feb49b0402ef0bbd72
mehdidotesk73 <mehdi.esk73@gmail.com>
2024-11-11 11:40:55 -0700

    cleanup gitignores again

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/backend-3834885064:8000";
+const apiUrl = "/choreo-apis/business-ops-software/backend/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
